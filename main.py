7304e2d8a59a89f3265a33510f4e91e58d8fe112
tomasdeambrosi <tomasdeambrosi@gmail.com>
2022-04-20 17:49:04 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+'''En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: 
+sumar, restar, multiplicar y dividir.
+
+Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis 
+que mostrarlos por consola.'''
+
+import calculadora.calculadora as operar
+
+print(operar.suma(1, 2))
+print(operar.resta(10, 5))
+print(operar.multiplicacion(100, 80))
+print(operar.division(480, 3))
